d93273194d6befc17993239b730506510bdc753d
JGranne <44633492+JGranne@users.noreply.github.com>
2021-10-04 19:29:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,22 @@ function compute()
     var r = document.getElementById("rate").value;
     var y = document.getElementById("years").value;
 
-    var date = new Date();
-    var year = date.getFullYear() + new Date(y).getFullYear();
-    var result = ((p*r)/100)*y;
-    
-    var text = "<p>If you deposit " + p + ",<br/>at an interest rate of " + r + "%.<br/>You will recieve an amount of " + result + ",<br/>in the year " + year + "</p>";
-    document.getElementById("result").innerHTML = text;
+    if (p == "")
+    {
+        alert("Amount must contain a positive number");
+    }
+    else if (p <= 0)
+    {
+        alert("Amount must be a positive number");       
+    }
+    else
+    {
+        var date = new Date();
+        var year = date.getFullYear() + new Date(y).getFullYear();
+        var result = ((p*r)/100)*y;
+        var text = "<p>If you deposit <span class='highlight'>" + p + "</span>,<br/>at an interest rate of <span class='highlight'>" + r + "%</span>.<br/>You will recieve an amount of <span class='highlight'>" + result + "</span>,<br/>in the year <span class='highlight'>" + year + "</span></p>";
+        document.getElementById("result").innerHTML = text;
+    }
 }
 
 function slide()
